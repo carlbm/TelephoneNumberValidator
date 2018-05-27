8e54b64d7c8316d548beee2cd804c5d5867f7e47
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-05-27 11:14:33 +0100

    Made the test assembly a friend Calling each cleaner directly

--- src/Tech.NumberValidator.Tests/NumberCleanup/ZeroAndPlusMismatch.cs
+++ src/Tech.NumberValidator.Tests/NumberCleanup/ZeroAndPlusMismatch.cs
@@ -1,5 +1,5 @@
 ﻿using FluentAssertions;
-using Tech.NumberValidator.Uk;
+using Tech.NumberValidator.NumberCleaners;
 using Xunit;
 
 namespace Tech.NumberValidator.Tests.NumberCleanup
@@ -7,10 +7,14 @@ namespace Tech.NumberValidator.Tests.NumberCleanup
     public class ZeroAndPlusMismatch
     {
         [Theory]
-        [InlineData("0+447926946745", "447926946745")]
+        [InlineData("0+447926946745", "+447926946745")]
+        [InlineData("044+7926946745", "+7926946745")]
+        [InlineData("+0447926946745", "+0447926946745")]
+        [InlineData("0447926946745", "0447926946745")]
         public void IdentifiesIncorrectlyPlacedPlus(string input, string expected)
         {
-            var actual = Clean.Number(input);
+            var telephoneNumber = new TelephoneNumber(input);
+            var actual = new DetectMismatchedPlus().TestAndClean(telephoneNumber).CleanedNumber;
 
             actual.Should().Be(expected);
         }

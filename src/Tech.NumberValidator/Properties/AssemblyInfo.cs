a3eff342ccdfae2ccbde9a9f5e28ee6f419c8389
Carl <carl@cabm.co.uk>
2024-04-25 15:49:49 +0100

    Upgraded to .net standard 2.0

--- src/Tech.NumberValidator/Properties/AssemblyInfo.cs
+++ src/Tech.NumberValidator/Properties/AssemblyInfo.cs
@@ -1,18 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Tech.NumberValidator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Techcellence Consulting")]
 [assembly: AssemblyProduct("Tech.NumberValidator")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
+
 [assembly: InternalsVisibleTo("Tech.NumberValidator.Tests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -22,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d8bef15f-a8ce-4ddc-84fa-f7c4fd82edc6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
